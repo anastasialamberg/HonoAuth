d5346aadb80b96b63d131b9638df56307a0c4942
anastasialamberg <anastasia.lamberg@gmail.com>
2025-11-22 22:08:41 +0200

    Add openAPI and browser checks

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
 import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/node-postgres";
+
 import * as schema from "./schema.ts";
 
 export const pool = new Pool({
