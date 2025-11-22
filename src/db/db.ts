accd0dda84744cb678dea00e1d2ebef972292894
anastasialamberg <anastasia.lamberg@gmail.com>
2025-11-22 21:49:51 +0200

    Create database tables and connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,12 +2,10 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.ts";
 
-
 export const pool = new Pool({
-    connectionString: Deno.env.get("DATABASE_URL")
-,
-    max: 10,
-    idleTimeoutMillis: 30000,
+  connectionString: Deno.env.get("DATABASE_URL"),
+  max: 10,
+  idleTimeoutMillis: 30000,
 });
 
-export const db = drizzle(pool, {schema, casing:"snake_case"});
+export const db = drizzle(pool, { schema, casing: "snake_case" });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,16 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/db.ts";
+import { openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-    }),
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    openAPI(),
+  ],
 });
